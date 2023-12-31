fd250a3c549655db1263adf252c42ff6f92fe19c
junaid1109 <junaidbhatti1109@gmail.com>
2023-12-31 21:25:33 +0500

    latest dist folder

--- src/main.js
+++ src/main.js
@@ -7,13 +7,13 @@ const app = createApp(App)
 app.mixin(request)
 // app.use(VueCookie)
 // app.provide('cookies', app.config.globalProperties.$cookies)
-app.config.globalProperties.$assets = 'http://localhost:5173/'
-app.config.globalProperties.$main = ''
+app.config.globalProperties.$assets = 'http://admin-stag.activefamilyhq.com/'
+// app.config.globalProperties.$main = ''
 app.config.globalProperties.$web = 'http://server.activefamilyhq.com'
-app.config.globalProperties.$auth = ''
+// app.config.globalProperties.$auth = ''
 
 // app.config.globalProperties.$assets = 'http://localhost:5173'
-// app.config.globalProperties.$main = 'http://server.activefamilyhq.com/api/v1/admin'
-// app.config.globalProperties.$web = 'http://server.activefamilyhq.com'
-// app.config.globalProperties.$auth = 'http://server.activefamilyhq.com/api/v1/auth'
+app.config.globalProperties.$main = 'http://server.activefamilyhq.com/api/v1/admin'
+app.config.globalProperties.$web = 'http://server.activefamilyhq.com'
+app.config.globalProperties.$auth = 'http://server.activefamilyhq.com/api/v1/auth'
 app.use(router).mount('#app');
